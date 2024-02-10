009b80144397f009bbdc0782c4fbf90aaa462455
Gabriel Richard <richard.gabriel150@yahoo.com.br>
2024-02-10 00:54:35 -0300

    parte 3 finalizada

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,68 @@
+import Button from '../Button'
+import {
+  CartContainer,
+  CartItem,
+  Overlay,
+  Prices,
+  Quantify,
+  Sidebar
+} from './styles'
+
+import starWars from '../../assets/images/star_wars.png'
+import Tag from '../Tag'
+import { RootReducer } from '../../store'
+import { useDispatch, useSelector } from 'react-redux'
+import { close, remove } from '../../store/reducers/cart'
+import { formataPreco } from '../ProductsList'
+
+const Cart = () => {
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <Sidebar>
+        <ul>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreco(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button" />
+            </CartItem>
+          ))}
+        </ul>
+        <Quantify>{items.length} jogo(s) no carrinho</Quantify>
+        <Prices>
+          Total de {formataPreco(getTotalPrice())}{' '}
+          <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button title="Clique aqui para continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </Sidebar>
+    </CartContainer>
+  )
+}
+
+export default Cart
